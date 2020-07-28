cc705c1af4dbe085f3ec5ea4a2996a9a0913b05c
Jonathan Hsieh <jonathanh1386@gmail.com>
2020-07-28 00:32:51 -0700

    did some stuff

---
+++ test.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import cassiopeia as cass
+import random
+import arrow
+from cassiopeia.core import Summoner, MatchHistory, Match
+from cassiopeia import Queue, Patch, Match, Season
+from cassiopeia.data import Role, Lane, Division, Tier
+import json
+import matplotlib.pyplot as plt
+
+conf = json.loads(open("config.json", "r").read())
+
+cass.set_riot_api_key(conf["API_KEY"])
+cass.set_default_region("NA")
+
+matches = cass.MatchHistory(summoner=Summoner(name="Tompson", region="NA"), queues=["ARAM"])
+print(matches[0].blue_team.participants)
+
+cass.configuration.settings.expire_sinks()
+cass.configuration.settings.clear_sinks()
+for sink in cass.configuration.settings.pipeline._sinks:
+    sink.expire(0)
+    sink.clear(0)
+
+print(matches[0].blue_team.participants)
+
+input()
+
+
+while(len(users) != 0):
+
+    cuser = users[0]
+    seenUsers.add(users[0])
+
+    users.pop(0)
+    matches = cass.MatchHistory(summoner=Summoner(name=cuser, region="NA"), queues=["ARAM"])
+    print(len(matches))
+
+    fullgames = json.loads(open("matches.dat", "r").read())
+    seen = set()
+    for g in fullgames:
+        seen.add(g["id"])
+
+    print("Already found " + str(len(fullgames)) + " matches...")
+
+    for match in matches[5:]:
+        try:
+            if(match.id in seen):
+                continue
+            seen.add(match.id)
+
+            gameObject = {
+                "id": match.id,
+                "blueteam": [x.champion.id for x in match.blue_team.participants],
+                "redteam": [x.champion.id for x in match.red_team.participants],
+                "result": int(match.blue_team.win)
+            }
+
+            for p in match.blue_team.participants:
+                if(p.summoner.name in seenUsers):
+                    continue
+                seenUsers.add(p.summoner.name)
+                users.append(p.summoner.name)
+
+            for p in match.red_team.participants:
+                if(p.summoner.name in seenUsers):
+                    continue
+                seenUsers.add(p.summoner.name)
+                users.append(p.summoner.name)
+
+            fullgames.append(gameObject)
+            print(gameObject, end="\n\n")
+        except KeyboardInterrupt:
+            raise
+        except:
+            continue
+
+    print("Updated to " + str(len(fullgames)) + " matches!")
+
+    open("matches.dat", "w").write(json.dumps(fullgames))
+
+
